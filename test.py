24d1ac7241a39d47365860daffb6640fb51a1cb0
peng7413 <a0843674028@gmail.com>
2023-11-10 18:00:33 +0800

    上傳測試程式

---
+++ test.py
@@ -0,0 +1,16 @@
+def CalculateBMI(height,kilogram):
+    print('%d %d' %(height,kilogram))
+    return 0
+def CalculateAVG(a,b):
+    print('%d %d' %(a,b))
+    return 0
+def CalculateStair(n):
+    print('%d' %n)
+    return 0
+
+def main():
+    CalculateBMI(170,65)
+    CalculateAVG(5,17)
+    CalculateStair(8)
+
+main()
