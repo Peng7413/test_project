92bcb4265b52753a6bc644fc398354eb8ac02dba
zxcasd89525 <zxcasd89525@gmail.com>
2023-11-15 23:14:57 +0800

    function edit.

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 def CalculateBMI(height,kilogram):
     result = kilogram/((height/100)*(height/100))
-    print('BMI:%d' %result)
-    return result
-def CalculateAVG(a,b):
-    print('%d %d' %(a,b))
-    return (a+b)//2
-def CalculateStair(n):
-    print('%d' %n)
+    print('%d' %result)
     return 0
+def CalculateAVG(a,b):
+    result = (a+b)//2
+    print('%d' %result)
+    return result
+def CalculateStair(n):
+    result = 1
+    for i in range(n):
+        result *= (i+1)
+    print('%d' %result)
+    return result
 
 def main():
     CalculateBMI(170,65)
