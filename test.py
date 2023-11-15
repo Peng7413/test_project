cb1d39f583c000435d4eedbc90996b0991cee622
peng7413 <a0843674028@gmail.com>
2023-11-15 23:17:36 +0800

    新增STAIR函式

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 def CalculateBMI(height,kilogram):
     result = kilogram/((height/100)*(height/100))
-    print('BMI:%d' %result)
     return result
 def CalculateAVG(a,b):
-    print('%d %d' %(a,b))
     return (a+b)//2
 def CalculateStair(n):
-    print('%d' %n)
-    return 0
+    if(n==1 or n==0):
+        return 1
+    elif(n>1):
+        return CalculateStair(n-1)+CalculateStair(n-2)
 
 def main():
-    CalculateBMI(170,65)
-    CalculateAVG(5,17)
-    CalculateStair(8)
+    print(CalculateBMI(170,65))
+    print(CalculateAVG(5,17))
+    print("我也是個廣東人，所以我們可能是老鄉"+str(CalculateStair(4)))
     print("測試")
 
 main()
